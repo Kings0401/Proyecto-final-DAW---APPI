c20b879a53f59f285147196bbe792d3886479327
Gabriela Reyes <gabrielarego@unisabana.edu.co>
2024-05-21 19:09:14 -0500

    Cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import userRoutes from './routes/userRoutes';
 import tweetRoutes from './routes/tweetRoutes';
-
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use('/user', userRoutes);
 app.use('/tweet', tweetRoutes);
